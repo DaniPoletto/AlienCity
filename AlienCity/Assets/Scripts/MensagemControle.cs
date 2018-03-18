cf4aec03d0286abfe97e80bbe9a5f92057be2e82
Daniela Poletto <danielapoletto_bep@hotmail.com>
2018-03-17 21:40:18 -0300

    Teste!
    Testando!

---
+++ AlienCity/Assets/Scripts/MensagemControle.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MensagemControle : MonoBehaviour {
+
+	// Use this for initialization
+	public GUIText gameOverTxt;
+	public GUIText restartTxt;
+	public GUIText venceuTxt;
+
+	public bool gameOver;
+	public bool restart;
+	public bool venceu;
+
+	void Start () {
+		gameOver = false;
+		restart = false;
+		venceu = false;
+		gameOverTxt.text = "";
+		restartTxt.text = "";
+		venceuTxt.text = "";
+	}
+	
+	void Update () {
+		if (gameOver) {
+			restartTxt.text = "Pressione 'R' para Restart";
+			restart = true;
+		}
+		if (restart) {
+			if(Input.GetKeyDown(KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	public void GameOver()
+	{
+		gameOverTxt.text = "Game Over!!!!";
+		gameOver = true;
+	}
+
+	public void Venceu()
+	{
+		venceuTxt.text = "Voce Venceu!!!";
+		restartTxt.text = "Pressione 'R' para Restart";
+		restart = true;
+	}
+}
---
+++ AlienCity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	private Animator anim;
+	private Rigidbody2D rb2d;
+
+	public Transform posPe;
+	[HideInInspector] public bool tocaChao = false;
+
+
+	public float Velocidade;
+	public float ForcaPulo = 1000f;
+	[HideInInspector] public bool viradoDireita = true;
+
+	public Image vida;
+	private MensagemControle MC;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		GameObject mensagemControleObject = GameObject.FindWithTag ("MensagemControle");
+		if (mensagemControleObject != null) {
+			MC = mensagemControleObject.GetComponent<MensagemControle> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Implementar Pulo Aqui! 
+	}
+
+	void FixedUpdate()
+	{
+		float translationY = 0;
+		float translationX = Input.GetAxis ("Horizontal") * Velocidade;
+		transform.Translate (translationX, translationY, 0);
+		transform.Rotate (0, 0, 0);
+		if (translationX != 0 && tocaChao) {
+			anim.SetTrigger ("corre");
+		} else {
+			anim.SetTrigger("parado");
+		}
+
+		//Programar o pulo Aqui! 
+
+		if (translationX > 0 && !viradoDireita) {
+			Flip ();
+		} else if (translationX < 0 && viradoDireita) {
+			Flip();
+		}
+
+	}
+	void Flip()
+	{
+		viradoDireita = !viradoDireita;
+		Vector3 escala = transform.localScale;
+		escala.x *= -1;
+		transform.localScale = escala;
+	}
+
+	public void SubtraiVida()
+	{
+		vida.fillAmount-=0.1f;
+		if (vida.fillAmount <= 0) {
+			MC.GameOver();
+			Destroy(gameObject);
+		}
+	}
+	
+}
